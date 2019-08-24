16e3978c4b3f4321c7bf51b86876850b3c49a119
wnsdyd0612 <54461186+wnsdyd0612@users.noreply.github.com>
2019-08-24 12:19:53 +0900

    Add files via upload

---
+++ output02.py
@@ -0,0 +1,5 @@
+yes_votes = 42_572_654
+no_votes = 43_132_495
+percentage = yes_votes / (yes_votes + no_votes)
+'{:-9} YES votes  {:2.2%}'.format(yes_votes, percentage)
+' 42572654 YES votes  49.67%'
---
+++ output03.py
@@ -0,0 +1,4 @@
+a = [1, 2, 3, 4] #=list
+b = ["first", "second", "third", "fourth"]
+c = a + b
+print("{0}   {1}   {2}".format(a[0],  b,  c[7]))
